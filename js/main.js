d365e93e6a266f94ae50dc784def3508428933af
Niloy Rudra <niloyrudra4249@gmail.com>
2018-12-31 18:28:43 +0600

    loader initiate two

--- js/main.js
+++ js/main.js
@@ -10,9 +10,16 @@ var loading = [
     { elements: $body, properties: { height: '100%' }, 
         options: { 
             complete: function() {
-                $( '.wrapper' ).velocity( 'transition.slideUpIn' );
-                $( 'img' ).velocity( 'transition.flipYIn' );
                 $( 'html' ).css( { background: 'white' } );
+
+                $( '.overlap-layer, .overlap-layer-two, .overlap-layer-three' ).velocity( { translateX: "0px" }, { duration: 500 } );
+
+                $( '.overlap-layer' ).velocity( { translateX: "100%" }, { delay: 0, duration: 700 } );
+                $( '.overlap-layer-two' ).velocity( { translateX: "100%" }, { delay: 300, duration: 1300 } );
+                $( '.overlap-layer-three' ).velocity( { translateX: "100%" }, { delay: 1250, duration: 500 } );
+
+                $( '.wrapper' ).velocity( 'transition.slideUpIn',{delay: 2000} );
+                $( 'img' ).velocity( 'transition.flipYIn', {delay: 2100} );
 
             } 
         }
